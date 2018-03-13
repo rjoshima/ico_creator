7ca0fb48116307a0f8f4ae438379087c95c3e21a
RJoshima <sojoy82@yahoo.co.jp>
2018-03-14 04:01:57 +0900

    fix: to be able to work token constract well

--- contracts/DappsToken.js
+++ contracts/DappsToken.js
@@ -2,10 +2,18 @@ var DappsToken = artifacts.require("./DappsToken.sol");
 
 
 // ここはinputでformからpostが走ったらnode.jsでtruffle developのshellを実行するようにするか
+// module.exports = (deployer) => {
+//     const initialSupply = ""
+//     const tokenName = ""
+//     const decimalUnits = ""
+//     const tokenSymbol = ""
+//     deployer.deploy(DappsToken, initialSupply, tokenName, decimalUnits, tokenSymbol);
+// }
+
 module.exports = (deployer) => {
-    const initialSupply = ""
-    const tokenName = ""
-    const decimalUnits = ""
-    const tokenSymbol = ""
+    const initialSupply = 100
+    const tokenName = "firstname"
+    const decimalUnits = 18
+    const tokenSymbol = "FN"
     deployer.deploy(DappsToken, initialSupply, tokenName, decimalUnits, tokenSymbol);
 }
--- contracts/DappsToken.sol
+++ contracts/DappsToken.sol
@@ -1,13 +1,36 @@
 pragma solidity ^0.4.18;
-import "zeppelin-solidity/contracts/token/StandardToken.sol";
+
+import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 
 contract DappsToken is StandardToken {
 
+    uint256 public totalSupply;
+    string public name;
+    uint8 public decimals;
+    string public symbol;
+
+// コンストラクタ
   function DappsToken(uint256 _initialAmount, string _tokenName, uint8 _decimalUnits, string _tokenSymbol) public {
-       balances[msg.sender] = _initialAmount;               // Give the creator all initial tokens
-       totalSupply = _initialAmount;                        // Update total supply
-       name = _tokenName;                                   // Set the name for display purposes
-       decimals = _decimalUnits;                            // Amount of decimals for display purposes
+       balances[msg.sender] = _initialAmount;             
+       totalSupply = _initialAmount;                    
+       name = _tokenName;                               
+       decimals = _decimalUnits;                         
        symbol = _tokenSymbol;    
   }
 }
+
+
+// pragma solidity ^0.4.18;
+// import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+
+// contract MyToken is StandardToken {
+//   string public name = "";
+//   string public symbol = "MTKN";
+//   uint public decimals = 18;
+
+//   function MyToken(uint initialSupply, string _tokenName) public {
+//     totalSupply_ = initialSupply;
+//     name = _tokenName;
+//     balances[msg.sender] = initialSupply;
+//   }
+// }
