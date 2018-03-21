a6cb1af08171599e435cf3f8a739d49bbcd4edb5
RJoshima <sojoy82@yahoo.co.jp>
2018-03-21 18:34:14 +0900

    add: form prototype

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,6 +1,43 @@
-new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue.js!'
-  }
-})
+// new Vue({
+//   el: '#app',
+//   data: {
+//     message: 'Hello Vue.js!'
+//   }
+// })
+
+
+Vue.use(VueForm, {
+  inputClasses: {
+     valid: 'form-control-success',
+     invalid: 'form-control-danger'
+   }
+ });
+  
+ new Vue({
+   el: '#app',
+   data: {
+     formstate: {},
+     model: {
+       name: '',
+       email: '',
+       phone: '',
+       department: null,
+     }
+   },
+   methods: {
+     fieldClassName: function (field) {
+       if(!field) {
+         return '';
+       }
+       if((field.$touched || field.$submitted) && field.$valid) {
+         return 'has-success';
+       }
+       if((field.$touched || field.$submitted) && field.$invalid) {
+         return 'has-danger';
+       }
+     },
+     onSubmit: function() {
+       console.log(this.formstate.$valid);
+     }
+   }
+ });
